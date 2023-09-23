97468798902eb853e668ea657ee69d272fecd205
Hannes <h@nnes.dev>
2023-09-23 09:34:56 +0800

    Add Bytes, Iterable and Iterator

--- type_aliases/__init__.py
+++ type_aliases/__init__.py
@@ -1,11 +1,14 @@
-from typing import TypeAlias
 import typing
+from typing import TypeAlias
 
 Any: TypeAlias = typing.Any
 Bool: TypeAlias = bool
+Bytes: TypeAlias = bytes
 Dict = dict
 Float: TypeAlias = float
 Int: TypeAlias = int
+Iterable = typing.Iterable
+Iterator = typing.Iterator
 List = list
 Literal = typing.Literal
 LiteralString: TypeAlias = typing.LiteralString
